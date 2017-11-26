12401918164a08f93e7a79703d8c7139a1c45211
Unknown <magahacka@gmail.com>
2017-11-26 19:25:51 +0100

    as

--- Python/MetalDetector/asdg.py
+++ Python/MetalDetector/asdg.py
@@ -1,68 +1,68 @@
-# -*- coding: utf-8 -*-
-from pyqtgraph.Qt import QtGui, QtCore
+import sys
+from random import randint, uniform
+import matplotlib.pyplot as plt
 import numpy as np
-import pyqtgraph as pg
-from PyQt5.QtWidgets import *
-from PyQt5.QtCore import *
-from PyQt5 import QtGui, QtCore
-from multiprocessing import Process, Manager, Queue
-import sched, time, threading
+import random
+import time
+import threading
+from tkinter import *
+values=160
 
-# This function is responsible for displaying the data
-# it is run in its own process to liberate main process
-def display(name,q):
-    app2 = QtGui.QApplication([])
+class App:
+    def __init__(self, master):
 
-    win2 = pg.GraphicsWindow(title="Basic plotting examples")
-    win2.resize(1000,600)
-    win2.setWindowTitle('pyqtgraph example: Plotting')
-    p2 = win2.addPlot(title="Updating plot")
-    curve = p2.plot(pen='y')
+        frame = Frame(master)
+        frame.pack()
 
-    x_np = []
-    y_np = []
+        self.button = Button(
+            frame, text="QUIT", fg="red",bg="green", command=frame.quit
+            )
+        self.button.pack(side=LEFT)
 
-    def updateInProc(curve,q,x,y):
-        item = q.get()
-        x.append(item[0])
-        y.append(item[1])
-        curve.setData(x,y)
+        self.hi_there = Button(frame, text="init Graph", command=self.say_hi)
+        self.hi_there.pack(side=LEFT)
+        self.fukoff=Button(frame, text="update graph",command=self.fukoff)
+        self.fukoff.pack(side=LEFT)
+        self.fukoff2=Button(frame, text="Kill graph",command=self.fukoff2)
+        self.fukoff2.pack(side=LEFT)
 
-    timer = QtCore.QTimer()
-    timer.timeout.connect(lambda: updateInProc(curve,q,x_np,y_np))
-    timer.start(50)
+    def say_hi(self):
+        print ("initializing graph")
+        fig, ax = plt.subplots()
+        line, = ax.plot(np.random.randn(100))
+        plt.xlabel('time (s)')
+        plt.ylabel('voltage (mV)')
+        plt.title('About as simple as it gets, folks')
+        plt.grid(True)
+        plt.show()
+    def fukoff(self):
+        print("Updating graph")
+        
+        starter=time.time()
+        
+        b=0
+        y_pos=np.array([])
+        while (b < values):
+            faf=random.randint(0,4092)
+            y_pos=np.append(y_pos,faf)
+            b=b+1
+        plt.cla()
+        plt.xlabel('time (s)')
+        plt.ylabel('voltage (mV)')
+        plt.title('About as simple as it gets, folks')
+        plt.grid(True)
+        plt.plot(y_pos)
+        plt.draw()
+        print(time.time()-starter)
+        
+    def fukoff2(self):
+        print("killing graph")
+        plt.close()
+    
+root = Tk()
 
-    QtGui.QApplication.instance().exec_()
+app = App(root)
 
-# This is function is responsible for reading some data (IO, serial port, etc)
-# and forwarding it to the display
-# it is run in a thread
-def io(running,q):
-    t = 0
-    while running.is_set():
-        s = np.sin(2 * np.pi * t)
-        t += 0.01
-        q.put([t,s])
-        time.sleep(0.01)
-    print("Done")
+root.mainloop()
+root.destroy() # optional; see description below
 
-if __name__ == '__main__':
-    q = Queue()
-    # Event for stopping the IO thread
-    run = threading.Event()
-    run.set()
-
-    # Run io function in a thread
-    t = threading.Thread(target=io, args=(run,q))
-    t.start()
-
-    # Start display process
-    p = Process(target=display, args=('bob',q))
-    p.start()
-    input("See ? Main process immediately free ! Type any key to quit.")
-    run.clear()
-    print("Waiting for scheduler thread to join...")
-    t.join()
-    print("Waiting for graph window process to join...")
-    p.join()
-    print("Process joined successfully. C YA !")
